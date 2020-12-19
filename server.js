bcc574c20c0e490eddb09d0f1fdfbbb0a7a02d0b
Coder Gautam <codergautam@Coders-Air.local>
2020-12-19 11:58:47 -0600

    v1 css

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static("./src/client"))
+app.use(express.static("./assets"))
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
